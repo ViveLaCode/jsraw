620ed0651ffe95fa2c562b97f88b1c1be75bf971
laughingclouds <zenithtaiga@gmail.com>
2022-05-29 20:07:47 +0530

    Create basic classes Created the basic clases, they haven't been tested yet

--- scripts/simple_fetch_get.js
+++
@@ -1,7 +0,0 @@
-// using QuizGameDBApi to host a server and 
-// fetching the response text
-module.exports.doFetch = async function() {
-  fetch("http://localhost:8080/")
-    .then(response => response.text())
-    .then(data => console.log(data));
-}
